dad998240067079c352a9c1326d5ab565ea0c6c9
AryanB <129508911+Aryan161103@users.noreply.github.com>
2024-12-09 21:39:18 +0530

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,3 +1,4 @@
+```cpp
 #include <LiquidCrystal.h>
 #include "DHT.h"
 
@@ -10,6 +11,7 @@ int led = 7;
 int fan = 10;
 int IRpin = 8;
 int lamp = 13;
+int co2Sensor = A1;
 char data = 0;
 
 void setup() {
@@ -20,59 +22,38 @@ void setup() {
   pinMode(fan, OUTPUT);
   pinMode(IRpin, INPUT);
   dht.begin();
-  lcd.begin(16, 2);  // Adjust these numbers for your LCD size
-  lcd.clear();       // Clear the LCD initially
-  lcd.print("Temperature:");
+  lcd.begin(16, 2);
+  lcd.clear();
+  lcd.print("Monitoring...");
 }
 
 void loop() {
-  // Read temperature from DHT sensor
   float t = dht.readTemperature();
+  int co2Level = analogRead(co2Sensor);
 
-  if (t < 20 || t > 40)
-    digitalWrite(buzzer, HIGH);
-  if(20<t || t<40)
-    digitalWrite(buzzer, LOW);
+  digitalWrite(buzzer, (t < 20 || t > 40 || co2Level > 800) ? HIGH : LOW);
+  digitalWrite(fan, (t > 35 || co2Level > 600) ? HIGH : LOW);
 
-  if (t > 35)
-    digitalWrite(fan, HIGH);
-  if (t < 25)
-    digitalWrite(fan, LOW);
-
-  // Display temperature on LCD
-  lcd.setCursor(0, 1);
-  lcd.print("                ");  // Clear previous temperature value
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 0);
   lcd.print("Temp: ");
   lcd.print(t);
-  lcd.print(" C");
+  lcd.print(" C  ");
 
-  // Read IR sensor value
-  int irvalue = digitalRead(IRpin);
-  if (irvalue == 0) {
-    digitalWrite(led, HIGH);
-  } else {
-    digitalWrite(led, LOW);
-  }
+  lcd.setCursor(0, 1);
+  lcd.print("CO2: ");
+  lcd.print(co2Level);
+  lcd.print(" ppm  ");
 
-  // Check for incoming data from serial communication
+  int irValue = digitalRead(IRpin);
+  digitalWrite(led, irValue == 0 ? HIGH : LOW);
+
   if (Serial.available() > 0) {
     data = Serial.read();
-    // Control components based on received data
-    if (data == '1')
-      digitalWrite(led, HIGH);
-    else if (data == '0')
-      digitalWrite(led, LOW);
-    else if (data == '3')
-      digitalWrite(lamp, LOW);
-    else if (data == '2')
-      digitalWrite(lamp, HIGH);
-    else if (data == '5')
-      digitalWrite(fan, LOW);
-    else if (data == '4')
-      digitalWrite(fan, HIGH);
+    digitalWrite(led, (data == '1') ? HIGH : (data == '0') ? LOW : digitalRead(led));
+    digitalWrite(lamp, (data == '2') ? HIGH : (data == '3') ? LOW : digitalRead(lamp));
+    digitalWrite(fan, (data == '4') ? HIGH : (data == '5') ? LOW : digitalRead(fan));
   }
 
-  // Add a delay to prevent rapid updates
   delay(1000);
 }
+```
